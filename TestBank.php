fd9b66ad26125da40caf44fc2c29901b06f5bef6
owenkx <owenkxia@hotmail.com>
2013-11-03 12:40:50 -0800

    PHP version with form action

---
+++ TestBank.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+        <head>
+        <title>File Uploader</title>
+        <meta charset="utf-8" />
+        </head>
+        <body>
+                <h1>Test Bank (Beta)</h1>
+                <form method="post">
+                        <input type="file" id="testfile" />
+                        <input type="submit" />
+        </body>
+</html>
