9675d65e5fedaf15f40ee6ddef296ea7134db120
owenkx <owenkxia@hotmail.com>
2013-11-03 12:51:39 -0800

    With the whole form she-bang

--- TestBank.php
+++ TestBank.php
@@ -8,6 +8,14 @@
                 <h1>Test Bank (Beta)</h1>
                 <form method="post">
                         <input type="file" id="testfile" />
+                        <label>Name</label>
+                        <input type='text' name='name' />
+                        <label>Class</label>
+                        <input type='text' name='class' />
+                        <label>Year</label>
+                        <input type='text' name='year' />
+                        <label>Quarter</label>
+                        <input type='text' name='quarter' />
                         <input type="submit" />
         </body>
 </html>
